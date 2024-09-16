80711cbea5fcf7be57ebc7ed80acd94472c9605f
Baptiste LECHAT <baptistelechat@outlook.fr>
2024-09-16 22:13:28 +0200

    ✨ (Tabs) - Separate image and background settings in separate tabs

--- src/components/image-generator/Sidebar.tsx
+++ src/components/image-generator/Sidebar.tsx
@@ -1,7 +1,5 @@
 import DownloadButton from "@/components/image-generator/DownloadButton";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import useImageGeneratorStore from "@/lib/store/imageGenerator.store";
-import { Input } from "../ui/input";
 import {
   Select,
   SelectContent,
@@ -9,7 +7,10 @@ import {
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "../ui/select";
+} from "@/components/ui/select";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import useImageGeneratorStore from "@/lib/store/imageGenerator.store";
+import { Input } from "../ui/input";
 import Border from "./section/Border";
 import Shadow from "./section/Shadow";
 import Size from "./section/Size";
@@ -52,60 +53,73 @@ const Sidebar = ({ generateImage }: ISidebarProps) => {
   };
 
   return (
-    <div className="flex w-full flex-col space-y-4 pb-4 md:w-96">
-      <h1 className="mb-4 text-center text-2xl font-bold">
-        🎨 Image Generator
-      </h1>
-      <ScrollArea className="size-full pr-4">
-        <div className="flex size-full flex-col gap-4">
-          <Input
-            type="text"
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-            placeholder="Enter your text"
-          />
-          <Input
-            type="color"
-            value={bgColor}
-            onChange={(e) => setBgColor(e.target.value)}
-            className="w-full"
-          />
-          <Border />
-          <Shadow />
-          <Size />
-          <Visibility />
+    <div className="flex h-full w-96 flex-col gap-4">
+      <Tabs defaultValue="image" className="flex grow flex-col">
+        <TabsList className="mb-4 grid w-full grid-cols-2">
+          <TabsTrigger value="image">Image</TabsTrigger>
+          <TabsTrigger value="background">Background</TabsTrigger>
+        </TabsList>
+        <div className="flex min-h-0 grow flex-col">
+          <TabsContent value="image" className="flex min-h-0 grow flex-col">
+            <ScrollArea className="max-h-[calc(100vh-375px)] grow">
+              <div className="flex flex-col gap-4 pr-4">
+                <Border />
+                <Shadow />
+                <Size />
+                <Visibility />
+                <Input
+                  type="text"
+                  value={text}
+                  onChange={(e) => setText(e.target.value)}
+                  placeholder="Enter your text"
+                />
+              </div>
+            </ScrollArea>
+          </TabsContent>
+          <TabsContent
+            value="background"
+            className="flex min-h-0 grow flex-col"
+          >
+            <ScrollArea className="max-h-[calc(100vh-375px)] grow">
+              <Input
+                type="color"
+                value={bgColor}
+                onChange={(e) => setBgColor(e.target.value)}
+                className="w-full"
+              />
+            </ScrollArea>
+          </TabsContent>
         </div>
-      </ScrollArea>
-      <Input
-        type="file"
-        accept="image/*"
-        onChange={(e) => {
-          handleImageChange(e);
-        }}
-        className="w-full"
-      />
-      <Select
-        value={`${width}x${height}`}
-        onValueChange={(value) => {
-          const [width, height] = value.split("x").map(Number);
-          setDimensions({ width, height });
-        }}
-      >
-        <SelectTrigger className="w-full">
-          <SelectValue placeholder="Select a size" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectGroup>
-            <SelectItem value="1920x1080">1920x1080 (16:9)</SelectItem>
-            <SelectItem value="1080x1080">1080x1080 (1:1)</SelectItem>
-            <SelectItem value="1080x1920">1080x1920 (9:16)</SelectItem>
-          </SelectGroup>
-        </SelectContent>
-      </Select>
-      <DownloadButton
-        generateImage={generateImage}
-        extraStyle="hidden md:flex"
-      />
+      </Tabs>
+      <div className="space-y-2">
+        <Input
+          type="file"
+          accept="image/*"
+          onChange={(e) => {
+            handleImageChange(e);
+          }}
+          className="w-full"
+        />
+        <Select
+          value={`${width}x${height}`}
+          onValueChange={(value) => {
+            const [width, height] = value.split("x").map(Number);
+            setDimensions({ width, height });
+          }}
+        >
+          <SelectTrigger className="w-full">
+            <SelectValue placeholder="Select a size" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectItem value="1920x1080">1920x1080 (16:9)</SelectItem>
+              <SelectItem value="1080x1080">1080x1080 (1:1)</SelectItem>
+              <SelectItem value="1080x1920">1080x1920 (9:16)</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+        <DownloadButton generateImage={generateImage} />
+      </div>
     </div>
   );
 };
