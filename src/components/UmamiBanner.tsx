61f6d0c64a391b01488185a265eb00dd8a714d38
Baptiste LECHAT <baptistelechat@outlook.fr>
2024-10-21 22:40:14 +0200

    ✨ (LocalStorage) - Using localStorage to manage user consent

--- src/components/UmamiBanner.tsx
+++ src/components/UmamiBanner.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { Button } from "@/components/ui/button";
 import {
   Sheet,
@@ -11,21 +11,46 @@ import {
 } from "@/components/ui/sheet";
 import { ChartSpline } from "lucide-react";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const UmamiBanner = () => {
-const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(true);
+
+  const EXPIRATION_TIME = 30 * 24 * 60 * 60 * 1000; // 30 days
+
+  const hasConsentExpired = (timestamp: string | null) => {
+    if (!timestamp) return true;
+
+    const currentTime = Date.now();
+    const consentTime = parseInt(timestamp, 10);
+    return currentTime - consentTime > EXPIRATION_TIME;
+  };
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const consentTimestamp = localStorage.getItem("umami.consent.timestamp");
+      setIsOpen(hasConsentExpired(consentTimestamp));
+    }
+  }, []);
 
   const handleAccept = () => {
+    if (typeof window !== "undefined") {
+      localStorage.removeItem("umami.disabled");
+      localStorage.setItem("umami.consent.timestamp", Date.now().toString());
+    }
     setIsOpen(false);
   };
 
   const handleDecline = () => {
+    if (typeof window !== "undefined") {
+      localStorage.setItem("umami.disabled", "1");
+      localStorage.setItem("umami.consent.timestamp", Date.now().toString());
+    }
     setIsOpen(false);
   };
 
   return (
-    <Sheet open={isOpen} onOpenChange={setIsOpen}>
+    <Sheet open={isOpen} onOpenChange={() => undefined}>
       <SheetContent side="bottom">
         <SheetHeader>
           <SheetTitle className="flex items-center gap-2 font-bold text-primary">
@@ -33,7 +58,7 @@ const [isOpen, setIsOpen] = useState(true);
             <p>Umami Analytics</p>
           </SheetTitle>
           <SheetDescription>
-            <div className="flex items-center gap-2">
+            <div className="mb-2 flex items-center gap-2">
               <Image
                 src={"/umami.png"}
                 alt="Umami Logo"
