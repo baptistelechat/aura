0a7cbe59b295f81d018a164a2b9c8573f519f22f
Baptiste LECHAT <baptistelechat@outlook.fr>
2024-10-27 00:23:20 +0200

    ✨ (StartLoader) - Moving loader animation in a separate component

--- src/app/image-generator/page.tsx
+++ src/app/image-generator/page.tsx
@@ -1,19 +1,15 @@
 "use client";
 import Preview from "@/app/image-generator/_components/Preview";
 import Sidebar from "@/app/image-generator/_components/Sidebar";
+import StartLoader from "@/components/StartLoader";
 import { hotkeys } from "@/lib/constant/hotkeys";
 import { useCustomHotKey } from "@/lib/hooks/useCustomHotKey";
 import { useMediaQuery } from "@/lib/hooks/useMediaQuery";
 import { useImageGeneratorStore } from "@/lib/store/imageGenerator.store";
-import { cn } from "@/lib/utils";
 import { updatePreviewSize } from "@/lib/utils/image-generator/updatePreviewSize";
 import { validateWatermark } from "@/lib/utils/image-generator/validateWatermark";
 import { MonitorSmartphone } from "lucide-react";
-import { Gugi } from "next/font/google";
-import Image from "next/image";
 import { useEffect, useState } from "react";
-
-const gugi = Gugi({ weight: "400", subsets: ["latin"] });
 
 const ImageGenerator = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -60,18 +56,7 @@ const ImageGenerator = () => {
 
   return (
     <div className="relative flex size-full gap-8 p-8">
-      {isLoading && (
-        <div className="absolute left-0 top-0 z-50 flex size-full items-center justify-center bg-background">
-          <div className="flex animate-bounce-slow flex-col items-center justify-center gap-4">
-            <Image src="/Logo.svg" alt="Logo" width={125} height={125} />
-            <p
-              className={cn("text-4xl font-bold text-primary", gugi.className)}
-            >
-              AURA
-            </p>
-          </div>
-        </div>
-      )}
+      {isLoading && <StartLoader />}
       <div className="hidden w-full gap-4 md:flex">
         <Sidebar />
         <Preview />
