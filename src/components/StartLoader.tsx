129ebc3b19bcd9d36cc4cc71a6cd782705e88d24
Baptiste LECHAT <baptistelechat@outlook.fr>
2024-10-27 11:40:05 +0100

    ✨ (Image) - Adding a image next to the logo

--- src/components/StartLoader.tsx
+++ src/components/StartLoader.tsx
@@ -3,16 +3,41 @@ import { gugi } from "@/lib/utils/fonts";
 import Image from "next/image";
 
 const StartLoader = () => {
+  const randomImageNumber = Math.floor(Math.random() * 10) + 1;
+
   return (
     <div className="absolute left-0 top-0 z-50 flex size-full items-center justify-center bg-gradient-to-bl from-[#0E4598] to-[#1573FE] p-4">
-      <div className="flex size-full flex-col items-center justify-center gap-4 rounded-xl bg-background">
-        <div className="flex flex-col items-center justify-center gap-4">
-          <Image src="/Logo.svg" alt="Logo" width={125} height={125} className="animate-bounce-slow" />
-          <p className={cn("text-5xl font-bold text-primary", gugi.className)}>
-            AURA
+      <div className="flex size-full rounded-xl bg-background">
+        <div className="flex h-full w-1/2 flex-col items-center justify-center gap-4">
+          <div className="flex flex-col items-center justify-center gap-4">
+            <Image
+              src="/Logo.svg"
+              alt="Logo"
+              width={150}
+              height={150}
+              className="animate-bounce-slow"
+            />
+            <p
+              className={cn("text-6xl font-bold text-[#0E4598]", gugi.className)}
+            >
+              AURA
+            </p>
+          </div>
+          <p className="text-3xl font-semibold text-[#1573FE]">
+            Create, Share, Inspire
           </p>
         </div>
-        <p className="text-2xl font-semibold text-[#1573FE]">Create, Share, Inspire</p>
+        <div className="flex h-full w-1/2 flex-col items-center justify-center gap-4">
+          <div className="relative size-full">
+            <Image
+              src={`/images/og-images/${randomImageNumber}.jpg`}
+              alt="Background"
+              layout="fill"
+              objectFit="cover"
+              className="rounded-r-xl grayscale"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
