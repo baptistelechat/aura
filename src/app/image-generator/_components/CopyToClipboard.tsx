262a072954c4ebbbfb412667459634015d9df5f3
Baptiste LECHAT <baptistelechat@outlook.fr>
2024-10-30 21:51:36 +0100

    ✨ (Format Select) - Add a format selector

--- src/app/image-generator/_components/CopyToClipboard.tsx
+++ src/app/image-generator/_components/CopyToClipboard.tsx
@@ -6,17 +6,12 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 import { useImageGeneratorStore } from "@/lib/store/imageGenerator.store";
-import { cn } from "@/lib/utils";
 import { getHotkeyById } from "@/lib/utils/hotkey/getHotkeyById";
 import { generateImage } from "@/lib/utils/image-generator/generateImage";
 import { ClipboardCopy } from "lucide-react";
 import Shortcut from "../../../components/keyboard/Shortcut";
 
-interface ICopyToClipboardProps {
-  extraStyle?: string;
-}
-
-const CopyToClipboard = ({ extraStyle }: ICopyToClipboardProps) => {
+const CopyToClipboard = () => {
   const isDownloading = useImageGeneratorStore((s) => s.general.isDownloading);
   const hotkey = getHotkeyById("copyToClipboard");
 
@@ -25,7 +20,7 @@ const CopyToClipboard = ({ extraStyle }: ICopyToClipboardProps) => {
       <TooltipTrigger asChild>
         <Button
           disabled={isDownloading}
-          className={cn("px-2", extraStyle)}
+          className="px-2"
           size="icon"
           onClick={() =>
             generateImage({
