d4997ba1dae495161c65dd41d76898cb287bd6c0
Baptiste LECHAT <baptistelechat@outlook.fr>
2024-10-30 22:02:44 +0100

    💄 (Icon) - Add a icon next to "Export format" title

--- src/app/image-generator/_components/FormatSelect.tsx
+++ src/app/image-generator/_components/FormatSelect.tsx
@@ -8,7 +8,7 @@ import {
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { useImageGeneratorStore } from "@/lib/store/imageGenerator.store";
 import { cn } from "@/lib/utils";
-import { Settings2 } from "lucide-react";
+import { FileCog, Settings2 } from "lucide-react";
 import sharp from "sharp";
 
 const FormatSelect = () => {
@@ -41,14 +41,16 @@ const FormatSelect = () => {
             setDimensions({ format: value as keyof sharp.FormatEnum })
           }
         >
-          <Label className="text-sm text-muted-foreground">Export Format</Label>
+          <Label className="flex items-center gap-2 text-sm text-muted-foreground">
+            <FileCog className="size-4" /> Export Format
+          </Label>
           {formats.map((format) => (
             <div key={format} className="flex items-center space-x-2">
               <RadioGroupItem
                 value={format}
                 id={format}
                 disabled={backgroundColor === "" && format !== "png"}
-              />
+              /> 
               <p
                 className={cn(
                   "text-sm text-muted-foreground",
