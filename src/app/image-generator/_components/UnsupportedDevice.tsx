ba9acda623b3cb5df37606efdf32613020341ac8
Baptiste LECHAT <baptistelechat@outlook.fr>
2024-11-02 18:01:52 +0100

    ♻️ (UnsupportedDevice) - Create an UnsupportedDevice component

---
+++ src/app/image-generator/_components/UnsupportedDevice.tsx
@@ -0,0 +1,47 @@
+import { browsers } from '@/lib/constant/browsers'
+import { MonitorSmartphone } from 'lucide-react'
+import Image from 'next/image'
+
+const UnsupportedDevice = () => {
+  return (
+    <div className="relative flex size-full gap-8 p-8">
+        <div className="flex size-full flex-col items-center justify-center gap-4">
+          <MonitorSmartphone className="size-40" />
+          <p className="text-center text-3xl font-bold">
+            For the best experience, please use Aura on a desktop browser
+          </p>
+          <p className="text-center text-lg text-muted-foreground">
+            Mobile and small devices are not supported, and some browsers are
+            not yet compatible.
+          </p>
+          <p className="text-center text-lg text-muted-foreground">
+            Meanwhile, we recommend using one of the desktop browsers suggested
+            below.
+          </p>
+          <div className="flex items-center justify-center gap-4">
+            {browsers.map((browser) => (
+              <div
+                key={browser.name.toLowerCase()}
+                className="flex flex-col items-center justify-center gap-2"
+              >
+                <Image
+                  src={browser.logo}
+                  alt={browser.name}
+                  width={40}
+                  height={40}
+                />
+                <p
+                  className="text-sm font-medium"
+                  style={{ color: browser.color }}
+                >
+                  {browser.name}
+                </p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default UnsupportedDevice
--- src/app/image-generator/page.tsx
+++ src/app/image-generator/page.tsx
@@ -1,16 +1,14 @@
 "use client";
 import Preview from "@/app/image-generator/_components/preview/Preview";
 import Sidebar from "@/app/image-generator/_components/sidebar/Sidebar";
-import { browsers } from "@/lib/constant/browsers";
 import { hotkeys } from "@/lib/constant/hotkeys";
 import { useCustomHotKey } from "@/lib/hooks/useCustomHotKey";
 import { useMediaQuery } from "@/lib/hooks/useMediaQuery";
 import { useImageGeneratorStore } from "@/lib/store/imageGenerator.store";
 import { updatePreviewSize } from "@/lib/utils/image-generator/updatePreviewSize";
 import { validateWatermark } from "@/lib/utils/image-generator/validateWatermark";
-import { MonitorSmartphone } from "lucide-react";
-import Image from "next/image";
 import { useEffect } from "react";
+import UnsupportedDevice from "./_components/UnsupportedDevice";
 
 const ImageGenerator = () => {
   useCustomHotKey(hotkeys);
@@ -46,43 +44,7 @@ const ImageGenerator = () => {
 
   if (!isDesktop || isSafari) {
     return (
-      <div className="relative flex size-full gap-8 p-8">
-        <div className="flex size-full flex-col items-center justify-center gap-4">
-          <MonitorSmartphone className="size-40" />
-          <p className="text-center text-3xl font-bold">
-            For the best experience, please use Aura on a desktop browser
-          </p>
-          <p className="text-center text-lg text-muted-foreground">
-            Mobile and small devices are not supported, and some browsers are
-            not yet compatible.
-          </p>
-          <p className="text-center text-lg text-muted-foreground">
-            Meanwhile, we recommend using one of the desktop browsers suggested
-            below.
-          </p>
-          <div className="flex items-center justify-center gap-4">
-            {browsers.map((browser) => (
-              <div
-                key={browser.name.toLowerCase()}
-                className="flex flex-col items-center justify-center gap-2"
-              >
-                <Image
-                  src={browser.logo}
-                  alt={browser.name}
-                  width={40}
-                  height={40}
-                />
-                <p
-                  className="text-sm font-medium"
-                  style={{ color: browser.color }}
-                >
-                  {browser.name}
-                </p>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
+      <UnsupportedDevice/>
     );
   }
 
